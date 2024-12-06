f0426f45c222ea71bc8a2ac4ce7fb446d07784a0
Fatah ari murgito <bsotoayam@gmail.com>
2024-12-06 11:24:48 +0700

    fix restore procedure

--- restore.py
+++ restore.py
@@ -7,8 +7,8 @@ import os
 import pandas as pd
 import subprocess
 
-def get_backup_history(a,b):
-    history_file = Path('./backup_history.csv')
+def get_backup_history(a,b,c):
+    history_file = Path(c)
     data = pd.read_csv(history_file)
     full_backup = data.query("Backup_Type == @a")
     incr_backup = data.query("Backup_Type == @b")
@@ -17,32 +17,35 @@ def get_backup_history(a,b):
     return full_backup, incr_exl_last,incr_last
 
 
-def prepare_backup():
-    full,increxl,incr_last = get_backup_history('full','incremental')
+def prepare_backup(historyfile):
+    full,increxl,incr_last = get_backup_history('full','incremental',historyfile)
     fbackup = full['Backup_Directory'].iloc[0]
     lbackup = incr_last['Backup_Directory'].iloc[0]
-    
+
+    #subprocess.run(["xtrabackup","--prepare","--apply-log-only",f"--target-dir={fbackup}"])
+    print(f"xtrabackup --prepare --apply-log-only --target-dir={fbackup}")
+
     for x in increxl.index:
         bdir = increxl['Backup_Directory'][x]
-        subprocess.run("xtrabackup","--prepare","--apply-log-only",f"target-dir={fbackup}",f"--incremental-dir{bdir}")
-    return fbackup
+        #subprocess.run(["xtrabackup","--prepare","--apply-log-only",f"--target-dir={fbackup}",f"--incremental-dir={bdir}"])
+        print(f"xtrabackup --prepare --apply-log-only --target-dir={fbackup} --incremental-dir={bdir}")
+
+    print(f"xtrabackup --prepare --target-dir={fbackup} --incremental-dir={lbackup}")
 
 
 def restore_backup(configfile,targetdir):
-    subprocess.run("xtrabackup","--defaults-file={configfile}","--copy-back","--target-dir={targetdir}")
+    #subprocess.run(["xtrabackup",f"--defaults-file={configfile}","--copy-back",f"--target-dir={targetdir}"])
+    print(f"xtrabakcup --defaults-file={configfile} --copy-back --target-dir{targetdir}")
 
 def run_script():
     parser = argparse.ArgumentParser(prog="Restore Backup",description="script to restore xtrabackup backup file mysql")
     parser.add_argument("-f","--history-file",required=True,help="this is the backup history file list in csv extension")
-    parser.add_argument("-d","--target-dir",required=True,help="target dir where the backup will be restored")
+    parser.add_argument("-d","--target-dir",required=True,help="base backup directory where full and incremental backup is located")
     parser.add_argument("-c","--config-file",required=True,help="mysql config file where the backup will be restored")
     args = vars(parser.parse_args())
-    print(args['history_file'])
-    print(args['target_dir'])
-    print(args['config_file'])
 
-
-
+    prepare_backup(args['history_file'])
+    restore_backup(args['config_file'],args['target_dir'])
 
 if __name__ == '__main__':
     run_script()
