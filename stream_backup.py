4025dfe7c325d57ba4bba0c8a066c85a90c64de8
keblingers <bsotoayam@gmail.com>
2025-03-21 14:41:58 +0700

    fix backup history file to be an argument

--- stream_backup.py
+++ stream_backup.py
@@ -41,18 +41,18 @@ def get_config(a,b):
         passwd = data['password'].iloc[0]
         return iphost,btype,bdir,username,today,yesterday,passwd
 
-def get_last_backup():
-    history_file = Path('./backup_history.csv')
-    if os.path.exists(history_file):
-        data = pd.read_csv(history_file)
+def get_last_backup(bhistory):
+    #history_file = Path('./backup_history.csv')
+    if os.path.exists(bhistory):
+        data = pd.read_csv(bhistory)
         last_backup = data['Backup_Type'].iloc[-1]
         last_day = data['Backup_Date'].iloc[-1]
         return last_backup,last_day
     else:
         print("backup file history not found")
 
-def update_history_file(btype,dirpath):
-    history_file = Path('./backup_history.csv')
+def update_history_file(btype,dirpath,bhistory):
+    #history_file = Path('./backup_history.csv')
     now = datetime.today().strftime('%Y-%m-%d')
     day_today = datetime.today().strftime('%A')
 
@@ -61,28 +61,28 @@ def update_history_file(btype,dirpath):
                     'Backup_Directory': [dirpath]}
     df = pd.DataFrame(backup_data)
         
-    if os.path.exists(history_file):
-        insert_history = df.to_csv(history_file,header=False,index=False,mode='a')
+    if os.path.exists(bhistory):
+        insert_history = df.to_csv(bhistory,header=False,index=False,mode='a')
     else:
         print("backup file history not found, creating backup file history now")
-        create_file = df.to_csv(history_file,index=False,mode='a')
+        create_file = df.to_csv(bhistory,index=False,mode='a')
         
 
 def check_backup_type():
         parser = argparse.ArgumentParser(prog='database backup',description='python script for database backup using percona xtrabackup. this script can do full and incremental backup with specify the backup type option')
         parser.add_argument('-t','--backup-type',required=True, help="backup type accepted value only incremental or full")
         parser.add_argument('-e','--env-file',required=True, help="env file for running backup")
+        parser.add_argument('-f','--backup-history',required=True, help="backup history catalog file")
         args = vars(parser.parse_args())
         btype = args['backup_type']
 
         if btype == 'full':
             print('===== processing full backup =====')
-            full_backup(args['backup_type'],args['env_file'])
+            full_backup(args['backup_type'],args['env_file'],args['backup_history'])
         elif btype == 'incremental':
             print('===== processing incremental backup =====')
-            last_backup,last_day = get_last_backup()
-            print(last_backup,last_day)
-            incremental_backup(last_backup,last_day,args['backup_type'],args['env_file'])
+            last_backup,last_day = get_last_backup(args['backup_history'])
+            incremental_backup(last_backup,last_day,args['backup_type'],args['env_file'],args['backup_history'])
         else:
              print("backup tyupe not found")
 
@@ -97,17 +97,17 @@ def check_backup_directory(dirpath,date):
            sys.exit(1)
 
 
-def full_backup(type,envpath):
+def full_backup(type,envpath,bhistory):
         host,btype,bdir,username,today,yesterday,passwd = get_config(type,envpath)
         check_backup_directory(bdir,today)
         backup_dir = f'{bdir}{today}'
         try:
           subprocess.run(["ssh",f"{host}","xtrabackup","--defaults-file=/etc/my.cnf","--login-path=local","--backup","--stream=xbstream",f"--target-dir={bdir}{today}","|","xbstream","-x","-C","/var/lib/data"])
-          update_history_file(type,backup_dir)
+          update_history_file(type,backup_dir,bhistory)
         except Exception as error:
             print(error)
 
-def incremental_backup(type,last,backup,envfile):
+def incremental_backup(type,last,backup,envfile,bhistory):
         btype,bdir,username,today,yesterday,passwd = get_config(backup,envfile)
         print(btype,bdir,username,today,yesterday,passwd)
         check_backup_directory(bdir,today)
@@ -116,14 +116,14 @@ def incremental_backup(type,last,backup,envfile):
              backuptype,backupdir,user,now,yday,passwd = get_config(type,envfile)
              try:
                  subprocess.run(["xtrabackup","--compress","--backup",f"--target-dir={bdir}{now}",f"--incremental-basedir={backupdir}{last}","-u",f"{user}",f"-p{passwd}"])
-                 update_history_file(backup,backup_dir)
+                 update_history_file(backup,backup_dir,bhistory)
              except Exception as error:
                  print(error)
         else:
             backuptype,backupdir,user,now,yday,passwd = get_config(type,envfile)
             try:
                 subprocess.run(["xtrabackup","--compress","--backup",f"--target-dir={bdir}{now}",f"--incremental-basedir={bdir}{yesterday}","-u",f"{username}",f"-p{passwd}"])
-                update_history_file(backup,backup_dir)
+                update_history_file(backup,backup_dir,bhistory)
             except Exception as error:
                 print(error)
 
